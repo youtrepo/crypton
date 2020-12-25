d00ebc2ea65c7cb6272d85e47271c835ef84f95f
youtsteve1@gmail.com <youtsteve123>
2020-12-25 22:42:36 +0300

    buy coin

--- app/Controllers/Http/BuyOfferController.js
+++ app/Controllers/Http/BuyOfferController.js
@@ -15,16 +15,41 @@ class BuyOfferController {
       let bch = await axios.get('https://api.coinbase.com/v2/exchange-rates?currency=BCH');
       let ltc = await axios.get('https://api.coinbase.com/v2/exchange-rates?currency=LTC');
       let dash = await axios.get('https://api.coinbase.com/v2/exchange-rates?currency=DASH');
-      console.log(dash)
+      let offers=buy_offers.toJSON();
+      for await (let info of offers){
+        let formatter = new Intl.NumberFormat('en-US', {
+          style: 'currency',
+          currency:info.currency,
 
+          // These options are needed to round to whole numbers if that's what you want.
+          //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+          //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+        });
+
+        switch (info.coin) {
+          case 'Btc':
+            info.rate=formatter.format(parseFloat(btc.data.data['rates'][info.currency])+info.margin/100*parseFloat(btc.data.data['rates'][info.currency]))
+            break;
+          case 'Bch':
+            info.rate=formatter.format(parseFloat(bch.data.data['rates'][info.currency])+info.margin/100*parseFloat(bch.data.data['rates'][info.currency]))
+            break;
+          case 'Ltc':
+            info.rate=formatter.format(parseFloat(ltc.data.data['rates'][info.currency])+info.margin/100*parseFloat(ltc.data.data['rates'][info.currency]))
+            break;
+          case 'Dash':
+            info.rate=formatter.format(parseFloat(dash.data.data['rates'][info.currency])+info.margin/100*parseFloat(dash.data.data['rates'][info.currency]))
+            break;
+
+        }
+      }
       return view.render('dashboard/buy_coin',{
-        offers:buy_offers.toJSON(),
+        offers:offers,
         param:request.url(),
         title:Env.get('TITLE'),
-        btc:btc,
-        bch:bch,
-        ltc:ltc,
-        dash:dash
+        btc:btc.data,
+        bch:bch.data,
+        ltc:ltc.data,
+        dash:dash.data,
       })
     } catch (e) {
       if (e.message === 'E_INVALID_SESSION: Invalid session') {
