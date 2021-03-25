0c5f667fc05a563d02b0d8783e7e29f37155ea6a
youtsteve1@gmail.com <youtsteve123>
2021-03-25 19:26:29 +0300

    dashboard 99% done

--- public/js/dashboard/dash_1.js
+++ public/js/dashboard/dash_1.js
@@ -9,3 +9,7 @@
   const ns = new PerfectScrollbar(document.querySelector('.notification-scroll'));
   //transactions scroll
   const ts = new PerfectScrollbar(document.querySelector('.widget-table-one .widget-content'));
+  //active trades
+  const at = new PerfectScrollbar(document.querySelector('.widget-table-two .widget-content'));
+  at.scrollTop=0
+
