eeb35432b1de32ca3d14b17a7907027566cfcae5
Your Name <you@example.com>
2021-04-15 10:30:00 +0100

    removed app/services/ws

--- app/Controllers/Http/LoginController.js
+++ app/Controllers/Http/LoginController.js
@@ -1,13 +1,12 @@
 'use strict'
 
 const Env = use('Env')
-const Ws=use('App/Services/Ws')
+
 class LoginController {
   async login({request,response,view,auth}){
     try {
       await auth.check()
       let user = await auth.getUser()
-      Ws.on('connection',(socket)=>{socket.join(user.email)})
       return response.redirect('/dashboard')
     }catch (e) {
       if (e.message==='E_INVALID_SESSION: Invalid session') {
