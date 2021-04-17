3bf428311093534eaf89fff4afc3b91f26ba59a3
youtsteve1@gmail.com <youtsteve123>
2021-04-17 14:56:25 +0300

    added new profile page

--- app/Controllers/Http/SettingController.js
+++ app/Controllers/Http/SettingController.js
@@ -1,11 +1,14 @@
 'use strict'
-
+const users=use('App/Models/User')
 class SettingController {
   async settings({request,response,view,auth}){
     try {
       await auth.check()
       let user = await auth.getUser()
-    return view.render('dashboard/settings')
+      let data=await users.findBy('email',user.email)
+    return view.render('dashboard/settings',{
+      user:user
+    })
     } catch (e) {
       if (e.message === 'E_INVALID_SESSION: Invalid session') {
         response.redirect('/login')
---
+++ public/settings/settings.js
@@ -0,0 +1,6 @@
+//function to update email on settings
+function updateEmail(){
+  $.ajax({
+
+  })
+}
