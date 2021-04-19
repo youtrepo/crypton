edf47c6f8698eec36d9c7746da092c97a7f84a02
Your Name <you@example.com>
2021-04-19 23:46:02 +0100

    fixed some spacing issues

--- app/Controllers/Http/ProfileController.js
+++ app/Controllers/Http/ProfileController.js
@@ -3,8 +3,14 @@
 class ProfileController {
   async profile({request, response, view,auth}) {
     try {
-      await auth.check()
-      return view.render('dashboard/profile')
+      await auth.check();
+      let user = await auth.getUser()
+      // console.log(user)
+      return view.render('dashboard/profile',{
+        fullname: user.$attributes.username,
+        country:  user.$attributes.country, 
+        email: user.$attributes.email
+      })
     } catch (error) {
       console.log(error)
       if (error.message === 'E_INVALID_SESSION: Invalid session') {
